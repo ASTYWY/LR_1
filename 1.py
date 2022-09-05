32b282af832e175b8e938f72d0552147809a34a3
ASTYWY <oytillogul@gmail.com>
2022-09-05 23:50:14 +0300

    1st ex

--- 1.py
+++ 1.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import sys
+from datetime import date
+
+if __name__ == '__main__':
+
+    def a(tag, s):
+        def b(s):
+            return "<"+tag+">"+s+"</"+tag+">";
+        return b(s)
+    print(a("html", "Test text"))
